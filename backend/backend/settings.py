a7b457ee9017e2ef44386cf69d46b6218dcf5515
tchalim <mtchalim@yulcom.ca>
2026-01-03 20:41:43 +0100

    configuration pro de ci

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "uploader_and_downloader",
-    "contenu.apps.ContentConfig",
+    "contenu.apps.ContenuConfig",
     "rest_framework",
     "django_celery_beat",
     "processing.apps.ProcessingConfig",
